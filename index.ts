90b8fd6754669f0475ba6b054aec71b09d626e26
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-07-25 15:12:14 +0800

    update

--- index.ts
+++ index.ts
@@ -7,13 +7,30 @@ import { linkmap } from './linkmap';
 import config from './config/config';
 import schedule from 'node-schedule';
 
-var loginCredentials = JSON.parse(fs.readFileSync("./config/auth.json", { encoding: 'utf-8', flag: 'r' }));
+var auth = JSON.parse(fs.readFileSync("./config/auth.json", { encoding: 'utf-8', flag: 'r' }));
 pixNode.common.setLanguage("zh-cn");
 
 linkmap.load();
 schedule.scheduleJob("30 * * * * ", () => {
     linkmap.save();
 })
+if (auth.expire_time - 60 < Math.round(Date.now())) {
+    pixNode.authenticate.refresh(auth.refresh_token, (res, err) => {
+        if (err) console.error(err);
+        auth = res;
+        fs.writeFileSync("./config/auth.json", JSON.stringify(auth), { encoding: 'utf-8', flag: 'w' });
+    })
+}
+schedule.scheduleJob(auth.expire_time - 60, () => {
+    pixNode.authenticate.refresh(auth.refresh_token, (res, err) => {
+        if (err) console.error(err);
+        auth = res;
+        fs.writeFileSync("./config/auth.json", JSON.stringify(auth), { encoding: 'utf-8', flag: 'w' });
+    })
+})
+if (auth.refresh_token == undefined) {
+    throw `LoginError: run "npm login" first`;
+}
 
 app.use(express.json({ limit: '50mb' }));
 
@@ -35,55 +52,18 @@ app.get('/linkmap', (req, res) => {
 })
 
 app.get('/recommend', (req, res) => {
-    function returnRecommend(
-        res: any,
-        login: pixNode.types.loginCredential
-    ) {
-        pixNode.fetch.recommendedIllustrations(login, { contentType: "ILLUSTRATION", }, (rel, err) => {
-            if (err) {
-                res.send(err);
-            } else res.send(rel);
-        })
-    }
-    if (loginCredentials.expire_time === undefined || loginCredentials.expire_time + 60 < Math.floor(Date.now() / 1000)) { // If session invaild
-        if (loginCredentials.refresh_token !== undefined) { // If have logged in before 
-            pixNode.authenticate.refresh(loginCredentials.refresh_token, (rel, err) => { // Refresh current session
-                if (err) {
-                    res.send(err);
-                }
-                if (rel) returnRecommend(res, rel);
-            })
-        } else { // Throw error to login
-            // res.status(500);
-            res.send({
-                code: 500,
-                response: { message: "Login credentials invalid." }
-            })
-            throw `LoginError: run "npm login" first`;
-        }
-    } else {
-        returnRecommend(res, loginCredentials);
-    }
+    pixNode.fetch.recommendedIllustrations(auth, { contentType: "ILLUSTRATION", }, (rel, err) => {
+        if (err) {
+            res.send(err);
+        } else res.send(rel);
+    })
 });
 
 /**
  * Get today's ranklist
  */
 app.get('/ranklist', (req, res) => {
-    function returnRanklist(
-        res: any,
-        login: pixNode.types.loginCredential,
-        mode?: "DAY" | "WEEK" | "MONTH" | "DAY_MALE" | "DAY_FEMALE" | "WEEK_ORIGINAL" | "WEEK_ROOKIE" | "DAY_MANGA" | undefined,
-        offset?: string
-    ) {
-        pixNode.fetch.illustrationRanking(login, { mode: mode }, (rel, err) => {
-            if (err) {
-                res.send(err);
-            } else res.send(rel);
-        })
-    }
 
-    const offset = typeof req.query.offset === 'string' ? req.query.offset : undefined;
     var dur = typeof req.query.duration == "string" ? req.query.duration : undefined;
     var duration: "DAY" | "WEEK" | "MONTH" | "DAY_MALE" | "DAY_FEMALE" | "WEEK_ORIGINAL" | "WEEK_ROOKIE" | "DAY_MANGA" | undefined
     if (dur == "DAY" || dur == "WEEK" || dur == "MONTH" || dur == "DAY_MALE" || dur == "DAY_FEMALE" || dur == "WEEK_ORIGINAL" || dur == "WEEK_ROOKIE" || dur == "DAY_MANGA") {
@@ -91,44 +71,17 @@ app.get('/ranklist', (req, res) => {
     } else {
         duration = "WEEK";
     }
-    if (loginCredentials.expire_time === undefined || loginCredentials.expire_time + 60 < Math.floor(Date.now() / 1000)) { // If session invaild
-        if (loginCredentials.refresh_token !== undefined) { // If have logged in before 
-            pixNode.authenticate.refresh(loginCredentials.refresh_token, (rel, err) => { // Refresh current session
-                if (err) {
-                    res.send(err);
-                }
-                if (rel) returnRanklist(res, rel, duration, offset);
-            })
-        } else { // Throw error to login
-            // res.status(500);
-            res.send({
-                code: 500,
-                response: { message: "Login credentials invalid." }
-            })
-            throw `LoginError: run "npm login" first`;
-        }
-    } else { // Session vaild, return data
-        returnRanklist(res, loginCredentials, duration, offset);
-    }
+    pixNode.fetch.illustrationRanking(auth, { mode: duration }, (rel, err) => {
+        if (err) {
+            res.send(err);
+        } else res.send(rel);
+    })
 })
 
 /**
  * Tag ranklist
  */
 app.get('/topInTag', (req, res) => {
-    function returnTagRanklist(
-        res: any,
-        login: pixNode.types.loginCredential,
-        keyword: string,
-        duration?: "LAST_DAY" | "LAST_WEEK" | "LAST_MONTH" | undefined,
-        offset?: number,
-    ) {
-        pixNode.fetch.searchForIllustration(login, keyword, { duration: duration, offset: offset, sort: "MALE_DESC" }, (rel: any, err: any) => {
-            if (err) {
-                res.send(err);
-            } else res.send(rel);
-        })
-    }
 
     const offset = typeof req.query.offset === "string" ? parseInt(req.query.offset) : undefined;
     var dur = typeof req.query.duration == "string" ? req.query.duration : undefined;
@@ -140,7 +93,6 @@ app.get('/topInTag', (req, res) => {
     }
     var keyword = "";
     if (req.query.keyword === undefined) {
-        // res.status(400);
         res.send({
             code: 400,
             response: { message: "Please specificate a keyword." }
@@ -149,50 +101,23 @@ app.get('/topInTag', (req, res) => {
     } else if (typeof req.query.keyword == "string") {
         keyword = req.query.keyword;
     } else {
-        // res.status(400);
         res.send({
             code: 400,
             response: { message: `Please specificate a valid keyword (Recieved ${req.query.keyword}).` }
         });
         return;
     }
-    if (loginCredentials.expire_time === undefined || loginCredentials.expire_time + 60 < Math.floor(Date.now() / 1000)) { // If session invaild
-        if (loginCredentials.refresh_token !== undefined) { // If have logged in before 
-            pixNode.authenticate.refresh(loginCredentials.refresh_token, (rel, err) => { // Refresh current session
-                if (err) {
-                    res.send(err);
-                }
-                if (rel) returnTagRanklist(res, rel, keyword, duration, offset);
-            })
-        } else { // Throw error to login
-            // res.status(500);
-            res.send({
-                code: 500,
-                response: { message: "Login credentials illegal." }
-            })
-            throw `LoginError: LoginError: run "npm login" first`;
-        }
-    } else { // Session vaild, return data
-        returnTagRanklist(res, loginCredentials, keyword, duration, offset);
-    }
+
+    pixNode.fetch.searchForIllustration(auth, keyword, { duration: duration, offset: offset, sort: "MALE_DESC" }, (rel, err) => {
+        if (err) {
+            res.send(err);
+        } else res.send(rel);
+    })
 })
 
 app.get("/illustrationDetail", (req, res) => {
-    function returnIllustrationDetail(
-        res: any,
-        login: pixNode.types.loginCredential,
-        keyword: string
-    ) {
-        pixNode.fetch.illustration(login, keyword, (rel: any, err: any) => {
-            if (err) {
-                res.send(err);
-            } else res.send(rel);
-        })
-    }
-
     var keyword = "";
     if (req.query.keyword === undefined) {
-        // res.status(400);
         res.send({
             code: 400,
             response: { message: "Please specificate the illustration ID." }
@@ -201,85 +126,41 @@ app.get("/illustrationDetail", (req, res) => {
     } else if (typeof req.query.keyword == "string" && isNaN(parseInt(req.query.keyword)) == false) {
         keyword = req.query.keyword;
     } else {
-        // res.status(400);
         res.send({
             code: 400,
             response: { message: `Please specificate a valid illustration ID (Recieved ${req.query.keyword})` }
         });
         return;
     }
-    if (loginCredentials.expire_time === undefined || loginCredentials.expire_time + 60 < Math.floor(Date.now() / 1000)) { // If session invaild
-        if (loginCredentials.refresh_token !== undefined) { // If have logged in before 
-            pixNode.authenticate.refresh(loginCredentials.refresh_token, (rel, err) => { // Refresh current session
-                if (err) {
-                    res.send(err);
-                }
-                if (rel) returnIllustrationDetail(res, rel, keyword);
-            })
-        } else { // Throw error to login
-            // res.status(500);
-            res.send({
-                code: 500,
-                response: { message: "Login credentials illegal." }
-            })
-            throw `LoginError: run "npm login" first`
-        }
-    } else { // Session vaild, return data
-        returnIllustrationDetail(res, loginCredentials, keyword);
-    }
+    pixNode.fetch.illustration(auth, keyword, (rel, err) => {
+        if (err) {
+            res.send(err);
+        } else res.send(rel);
+    })
 })
 
 app.get("/creatorIllustrations", (req, res) => {
-    function returncreatorIllustrations(
-        res: any,
-        login: pixNode.types.loginCredential,
-        keyword: number,
-        offset?: number
-    ) {
-        pixNode.fetch.userIllustrations(login, keyword, { contentType: "ILLUSTRATION", offset: offset }, (rel: any, err: any) => {
-            if (err) {
-                res.send(err);
-            } else res.send(rel);
-        })
-    }
-
     var keyword: number;
     if (req.query.keyword === undefined) {
-        // res.status(400);
         res.send({
             code: 400,
-            response: { message: "Please specificate the illustration ID." }
+            response: { message: "Please specificate the user ID." }
         });
         return;
     } else if (typeof req.query.keyword == "string" && isNaN(parseInt(req.query.keyword)) == false) {
         keyword = parseInt(req.query.keyword);
     } else {
-        // res.status(400);
         res.send({
             code: 400,
-            response: { message: `Please specificate a valid illustration ID (Recieved ${req.query.keyword}).` }
+            response: { message: `Please specificate a valid user ID (Recieved ${req.query.keyword}).` }
         });
         return;
     }
-    if (loginCredentials.expire_time === undefined || loginCredentials.expire_time + 60 < Math.floor(Date.now() / 1000)) { // If session invaild
-        if (loginCredentials.refresh_token !== undefined) { // If have logged in before 
-            pixNode.authenticate.refresh(loginCredentials.refresh_token, (rel, err) => { // Refresh current session
-                if (err) {
-                    res.send(err);
-                }
-                if (rel) returncreatorIllustrations(res, rel, keyword);
-            })
-        } else { // Throw error to login
-            // res.status(500);
-            res.send({
-                code: 500,
-                response: { message: "Login credentials illegal." }
-            })
-            throw `LoginError: run "npm login" first`
-        }
-    } else { // Session vaild, return data
-        returncreatorIllustrations(res, loginCredentials, keyword);
-    }
+    pixNode.fetch.userIllustrations(auth, keyword, { contentType: "ILLUSTRATION" }, (rel, err) => {
+        if (err) {
+            res.send(err);
+        } else res.send(rel);
+    })
 })
 
 app.listen(port, () => {
